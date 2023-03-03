64b12d88d52e2415a3f6b9b2212f4d3772965279
WarwarLouay <warwarlouay@gmail.com>
2023-03-03 15:29:34 +0200

    g

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -526,7 +526,7 @@ const Chat = () => {
                 <Stack direction="row" spacing={2}>
                   <div
                     className="arrowBack"
-                    style={{ alignItems: "center", display: "flex", position: "fixed" }}
+                    style={{ alignItems: "center", display: "flex" }}
                   >
                     <ArrowBackIcon
                       onClick={() => {
